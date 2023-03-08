7ef2e626bc54df2e9b94ed0a5d4af9491c2ac13f
jhondeser <jhonatan.mog2829@gmail.com>
2023-03-08 18:32:37 +0100

    subo clase de variables

---
+++ index.js
@@ -0,0 +1,101 @@
+
+//creamos una variable con un dato constante y intentamos reasignarla 
+//nos sale un error, no es posible reasignar una variable constante
+
+// const carro = 'ferrari'
+
+// console.log(carro);
+
+// carro = 'toyota';
+
+// console.log(carro);
+
+//creamos una variable let 
+//let si me permite reasignar el valor/dato a una variable
+
+//let fruta = 'banana'
+
+// console.log(fruta);
+
+// fruta = 'manzana'
+
+// console.log(fruta);
+
+//ej
+
+// const zapatillas = 'asics'
+
+// let años = '22'
+
+// //un dia despue, leandro cumple años
+
+// años = '23'
+
+// //pasa el tiempo y quiero ver la informacion dentro de edad y zapatillas
+
+// console.log(años, zapatillas);
+
+// console.log('años', años);
+
+// ej: crear una base de datos para un cliente que guarde num, email, nombre y edad
+
+// const nombre = 'pepito suarez'
+// let num = 6598745
+// let email = 'pepito@gmail.com'
+// let edad = 30
+
+// console.log('su nombre es', nombre,'su numero es', num,'su email es', email,'y su edad es', edad);
+
+// //podemos concatenar nuestros datos
+
+// console.log('su nombre es ' + nombre + 'su numero es ' + num + 'su email es ' + email + 'y su edad es ' + edad);
+
+//Numbers
+
+// console.log(5);
+
+// console.log('5');
+
+// //concatenar un number y un string
+// //siempre que se concatene un texto con un numero va a devolver un string
+
+// console.log(5 + "5");
+
+// console.log(5 + 5);
+
+// console.log(typeof(5 + 5));
+
+// //string 
+
+// console.log('hola mundo');
+// console.log(typeof('hola mundo'));
+
+// console.log('hola'+'pepe');
+
+// //boolean
+
+// console.log(true);
+// console.log(false);
+
+// console.log(typeof(true));
+
+// //null
+
+// let pais = null;
+// pais = 'españa'
+
+// console.log(pais);
+
+// //undefined
+
+// let carro;
+
+// console.log(carro);
+
+//creando nuestra primera lista
+
+let frutas = ['banana', 'fresas', 'manzana', 'mango']
+
+console.log(frutas);
+
+console.log(typeof(frutas));
